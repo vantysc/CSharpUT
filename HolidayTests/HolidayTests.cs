646579b0f1eada7efd2cb7e48e0e9d5a7e07dbf8
Joey Chen <jay.hatelove@gmail.com>
2020-07-12 17:46:18 +0800

    [Holiday]add failed test "today is xmas"

---
+++ HolidayTests/HolidayTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Lib;
+using NUnit.Framework;
+
+namespace HolidayTests
+{
+    [TestFixture]
+    public class HolidayTests
+    {
+        [Test]
+        public void today_is_xmas()
+        {
+            var holiday = new Holiday();
+            Assert.AreEqual("Merry Xmas", holiday.SayHello());
+        }
+    }
+}
