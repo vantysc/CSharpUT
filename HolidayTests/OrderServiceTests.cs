e5dbcec49210f956accb9ec571a02d9a28d5e013
Joey Chen <jay.hatelove@gmail.com>
2020-07-12 18:45:18 +0800

    [Order]pass test by extract and override 2 dependency

--- HolidayTests/OrderServiceTests.cs
+++ HolidayTests/OrderServiceTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using Lib;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace HolidayTests
 {
@@ -8,10 +11,50 @@ namespace HolidayTests
         [Test]
         public void syncBookOrders_3_orders_only_2_book_order()
         {
-            // hard to isolate dependency to unit test
+            var target = new OrderServiceForTest();
+            target.Orders = new List<Order>
+                            {
+                                new Order() {Type = "Book"},
+                                new Order() {Type = "CD"},
+                                new Order() {Type = "Book"},
+                            };
 
-            //var target = new OrderService();
-            //target.SyncBookOrders();
+            var bookDao = Substitute.For<IBookDao>();
+
+            target.BookDao = bookDao;
+            target.SyncBookOrders();
+
+            bookDao.Received(2)
+                   .Insert(Arg.Is<Order>(order => order.Type == "Book"));
+
+            bookDao.DidNotReceive()
+                   .Insert(Arg.Is<Order>(order => order.Type == "CD"));
+        }
+    }
+
+    public class OrderServiceForTest : OrderService
+    {
+        private List<Order> _orders;
+        private IBookDao _bookDao;
+
+        protected override IBookDao GetBookDao()
+        {
+            return _bookDao;
+        }
+
+        public List<Order> Orders
+        {
+            set => _orders = value;
+        }
+
+        public IBookDao BookDao
+        {
+            set => _bookDao = value;
+        }
+
+        protected override List<Order> GetOrders()
+        {
+            return _orders;
         }
     }
 }
--- Lib/OrderService.cs
+++ Lib/OrderService.cs
@@ -17,14 +17,19 @@ namespace Lib
             // only get orders of book
             var ordersOfBook = orders.Where(x => x.Type == "Book");
 
-            var bookDao = new BookDao();
+            var bookDao = GetBookDao();
             foreach (var order in ordersOfBook)
             {
                 bookDao.Insert(order);
             }
         }
 
-        private List<Order> GetOrders()
+        protected virtual IBookDao GetBookDao()
+        {
+            return new BookDao();
+        }
+
+        protected virtual List<Order> GetOrders()
         {
             // parse csv file to get orders
             var result = new List<Order>();
@@ -75,7 +80,12 @@ namespace Lib
         public string CustomerName { get; set; }
     }
 
-    public class BookDao
+    public interface IBookDao
+    {
+        void Insert(Order order);
+    }
+
+    public class BookDao : IBookDao
     {
         public void Insert(Order order)
         {
