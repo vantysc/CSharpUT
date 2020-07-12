177fe1bc8eba060006eb48c0233e5ecf3945ecf0
Joey Chen <jay.hatelove@gmail.com>
2020-07-12 17:52:29 +0800

    [Holiday]refactor test

--- HolidayTests/HolidayTests.cs
+++ HolidayTests/HolidayTests.cs
@@ -1,19 +1,39 @@
-﻿using System;
+﻿#region
+
+using System;
 using Lib;
 using NUnit.Framework;
+
+#endregion
 
 namespace HolidayTests
 {
     [TestFixture]
     public class HolidayTests
     {
+        private HolidayForTest _holiday;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _holiday = new HolidayForTest();
+        }
+
         [Test]
         public void today_is_xmas()
         {
-            var holiday = new HolidayForTest();
-            holiday.Today = new DateTime(2020, 12, 25);
+            GivenToday(12, 25); 
+            ResponseShouldBe("Merry Xmas");
+        }
 
-            Assert.AreEqual("Merry Xmas", holiday.SayHello());
+        private void ResponseShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, _holiday.SayHello());
+        }
+
+        private void GivenToday(int month, int day)
+        {
+            _holiday.Today = new DateTime(2020, month, day);
         }
     }
 
