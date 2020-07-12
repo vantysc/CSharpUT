2e9783138bebc16ec479e9ead0ba9cfb9943df0a
Joey Chen <jay.hatelove@gmail.com>
2020-07-12 17:39:04 +0800

    init

---
+++ HolidayTests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HolidayTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
