52cb9cc1261a5866fc84ce7571320b636f1b49d4
Joey Chen <jay.hatelove@gmail.com>
2020-07-12 18:03:20 +0800

    [Otp]init workshop

---
+++ Lib/AuthenticationService.cs
@@ -0,0 +1,69 @@
+﻿namespace Lib
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class AuthenticationService
+    {
+        public bool IsValid(string account, string password)
+        {
+            // 根據 account 取得自訂密碼
+            var profileDao = new ProfileDao();
+            var passwordFromDao = profileDao.GetPassword(account);
+
+            // 根據 account 取得 RSA token 目前的亂數
+            var rsaToken = new RsaTokenDao();
+            var randomCode = rsaToken.GetRandom(account);
+
+            // 驗證傳入的 password 是否等於自訂密碼 + RSA token亂數
+            var validPassword = passwordFromDao + randomCode;
+            var isValid = password == validPassword;
+
+            if (isValid)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class ProfileDao
+    {
+        public string GetPassword(string account)
+        {
+            return Context.GetPassword(account);
+        }
+    }
+
+    public static class Context
+    {
+        public static Dictionary<string, string> profiles;
+
+        static Context()
+        {
+            profiles = new Dictionary<string, string>();
+            profiles.Add("joey", "91");
+            profiles.Add("mei", "99");
+        }
+
+        public static string GetPassword(string key)
+        {
+            return profiles[key];
+        }
+    }
+
+    public class RsaTokenDao
+    {
+        public string GetRandom(string account)
+        {
+            var seed = new Random((int) DateTime.Now.Ticks & 0x0000FFFF);
+            var result = seed.Next(0, 999999).ToString("000000");
+            Console.WriteLine("randomCode:{0}", result);
+
+            return result;
+        }
+    }
+}
