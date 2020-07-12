0e4591d952269db9ba30d58a845c21adf8dcfb13
Joey Chen <jay.hatelove@gmail.com>
2020-07-12 18:48:50 +0800

    [Order]refactor test

--- HolidayTests/OrderServiceTests.cs
+++ HolidayTests/OrderServiceTests.cs
@@ -1,46 +1,70 @@
-﻿using System.Collections.Generic;
+﻿#region
+
+using System.Collections.Generic;
+using System.Linq;
 using Lib;
 using NSubstitute;
 using NUnit.Framework;
+
+#endregion
 
 namespace HolidayTests
 {
     [TestFixture]
     public class OrderServiceTests
     {
+        private IBookDao _bookDao;
+        private OrderServiceForTest _target;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _target = new OrderServiceForTest();
+            _bookDao = Substitute.For<IBookDao>();
+            _target.BookDao = _bookDao;
+        }
+
         [Test]
         public void syncBookOrders_3_orders_only_2_book_order()
         {
-            var target = new OrderServiceForTest();
-            target.Orders = new List<Order>
-                            {
-                                new Order() {Type = "Book"},
-                                new Order() {Type = "CD"},
-                                new Order() {Type = "Book"},
-                            };
+            GivenOrders(
+                new Order {Type = "Book"},
+                new Order {Type = "CD"},
+                new Order {Type = "Book"});
 
-            var bookDao = Substitute.For<IBookDao>();
+            WhenSyncBookOrders();
 
-            target.BookDao = bookDao;
-            target.SyncBookOrders();
+            ShouldInsertOrder(2, "Book");
+            ShouldNotInsertOrder("CD");
+        }
 
-            bookDao.Received(2)
-                   .Insert(Arg.Is<Order>(order => order.Type == "Book"));
+        private void ShouldNotInsertOrder(string orderType)
+        {
+            _bookDao.DidNotReceive()
+                    .Insert(Arg.Is<Order>(order => order.Type == orderType));
+        }
 
-            bookDao.DidNotReceive()
-                   .Insert(Arg.Is<Order>(order => order.Type == "CD"));
+        private void ShouldInsertOrder(int times, string orderType)
+        {
+            _bookDao.Received(times)
+                    .Insert(Arg.Is<Order>(order => order.Type == orderType));
+        }
+
+        private void WhenSyncBookOrders()
+        {
+            _target.SyncBookOrders();
+        }
+
+        private void GivenOrders(params Order[] orders)
+        {
+            _target.Orders = orders.ToList();
         }
     }
 
     public class OrderServiceForTest : OrderService
     {
-        private List<Order> _orders;
         private IBookDao _bookDao;
-
-        protected override IBookDao GetBookDao()
-        {
-            return _bookDao;
-        }
+        private List<Order> _orders;
 
         public List<Order> Orders
         {
@@ -50,6 +74,11 @@ namespace HolidayTests
         public IBookDao BookDao
         {
             set => _bookDao = value;
+        }
+
+        protected override IBookDao GetBookDao()
+        {
+            return _bookDao;
         }
 
         protected override List<Order> GetOrders()
