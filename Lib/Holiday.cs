7b242d58f3f3316f6b84278d56f851dbd8f16056
Joey Chen <jay.hatelove@gmail.com>
2020-07-12 17:44:53 +0800

    [Holiday]complete production code

---
+++ Lib/Holiday.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lib
+{
+    public class Holiday
+    {
+        public string SayHello()
+        {
+            var today = DateTime.Today;
+            if (today.Month == 12 && today.Day == 25)
+            {
+                return "Merry Xmas";
+            }
+
+            return "Today is not Xmas";
+        }
+    }
+}
